c78399cf93a2eb367732928d6eff64491afa8828
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2023-01-09 17:33:41 -0800

    Initial Commit

---
+++ MoteNoteApp/MoteNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoteNoteAppApp.swift
+//  MoteNoteApp
+//
+//  Created by Bernie Graves on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoteNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
